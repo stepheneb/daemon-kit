a3e0cdfa4f2dfd7cc31aa7715249730b5e59ff75
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-09-06 12:55:45 -0400

    remove spec test for DaemonKit::ErrorHandlers::Mail
    Mail was removed in commit 885432703...

--- spec/error_handlers_spec.rb
+++ spec/error_handlers_spec.rb
@@ -2,22 +2,3 @@ require File.expand_path('../spec_helper',  __FILE__)
 
 describe DaemonKit::Safety do
 end
-
-describe DaemonKit::ErrorHandlers::Mail do
-  it "should send an email report" do
-    conf = Object.new
-    conf.stubs(:daemon_name).returns('test')
-    DaemonKit.stubs(:configuration).returns(conf)
-
-    fake_smtp = Object.new
-    fake_smtp.expects(:start).with('localhost.localdomain', nil, nil, nil)
-    Net::SMTP.expects(:new).with('localhost', 25).returns(fake_smtp)
-
-    begin
-      raise RuntimeError, "specs don't fail :)"
-    rescue => e
-      handler = DaemonKit::ErrorHandlers::Mail.instance
-      handler.handle_exception( e )
-    end
-  end
-end
